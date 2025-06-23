bec0df4f78e6610713d8a8c9755a906ca02a5b2d
silenciadorsilencioso <aguhp086@gmail.com>
2025-06-23 20:31:52 +0000

    Fix: use single export in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/static';
-import node from '@astrojs/node';
 
-export default defineConfig({
-  output: 'server',
-  adapter: node({ mode: 'standalone' }),
-});
- // Importar el adaptador de Vercel
-
-// https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: vercel(), // Usar el adaptador importado
-  integrations: [tailwind()],
+  adapter: vercel(),
+  integrations: [tailwind()]
 });
